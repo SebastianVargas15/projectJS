e92b20f51e8579be8bea0d9dc339c6e94400ac69
sebavargas15 <carlos.vargas@estudiantes.utec.edu.uy>
2022-08-29 22:58:49 -0300

    commit

--- main.js
+++ main.js
@@ -1,71 +1,86 @@
-//Swal.fire("Hola mundo");
-
-
 let usuarios = [];
-
-
-function registro(user, email, password,name,lastname) {
+Swal.fire("Bienvenido");
+function registro(user, email, password, name, lastname) {
   this.user = user;
   this.email = email;
   this.password = password;
   this.name = name;
   this.lastname = lastname;
 }
-usuarios.push(new registro("admin", "admin@gmail.com", "Z2XHJ8LKW", "sebastian", "vargas"));
-usuarios.push(new registro("designer", "designer@gmail.com", "462226819tecH", "gimena", "lopez"));
-//usuarios.forEach((element) => console.log(element.user));
+usuarios.push(
+  new registro("admin", "admin@gmail.com", "Z2XHJ8LKW", "sebastian", "vargas")
+);
+usuarios.push(
+  new registro(
+    "designer",
+    "designer@gmail.com",
+    "462226819tecH",
+    "gimena",
+    "lopez"
+  )
+);
+usuarios.forEach((element) => console.log(element.user));
 
 
 
+
+localStorage.setItem("myusuarios", JSON.stringify(usuarios));
+let myusuarios = localStorage.getItem("myusuarios");
 function registrarse() {
   let user = document.getElementById("username").value;
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
   let name = document.getElementById("name").value;
   let lastname = document.getElementById("lastname").value;
-  usuarios.push(new registro(user,email,password,name,lastname));
+  let userExist = false;
+  usuarios.forEach((element) => {
+    if (element.user == user) {
+      userExist = true;
+    }
+  });
+  if (userExist) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "El usuario ya existe!",
+    });
+  } else {
+    usuarios.push(new registro(user, email, password, name, lastname));
+    localStorage.setItem("myusuarios", JSON.stringify(usuarios));
+    setTimeout(() => {
+      conditional = true;
+      Swal.fire({
+        title: "Cargando....",
+        text: "Espere un momento",
+      });
+    }, 0);
+    setTimeout(() => {
+      Swal.fire({
+        icon: "success",
+        title: "Registro Exitoso",
+        text: "Bienvenido",
+      });
+      conditional = false;
+    }, 2000);
+    console.log(usuarios);
+  }
 }
-
 const regSubmit = document.getElementById("btnsubmit");
 regSubmit.addEventListener("click", registrarse);
 console.log(usuarios);
 
+ // use promise to wait for html to load
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOM fully loaded and parsed");
+  let myusuarios = localStorage.getItem("myusuarios");
+  usuarios = JSON.parse(myusuarios);
+  console.log(usuarios);
+}
+);
 
-
-
-
-
-// function loaddata() {
-//   let users = JSON.parse(localStorage.getItem("users"));
-//   users.forEach((element) => console.log(element));
-// }
-// localStorage.setItem("users", JSON.stringify(usuarios));
-
-// if (typeof Storage !== "undefined") {
-//   console.log("LocalStorage Dispo");
-// } else {
-//   console.log("LocalStorage no soportado");
-// }
-// localStorage.setItem("creador", "Sebastian Vargas");
-// console.log(localStorage.getItem("creador"));
-// loaddata();
-
-// fetchedData = fetch("https://jsonplaceholder.typicode.com/users")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//     data.forEach((element) => {
-//       console.log(element.name);
-//     });
-//   });
-
-// ajax = new XMLHttpRequest();
-// ajax.open("GET", "https://jsonplaceholder.typicode.com/users");
-// ajax.send();
-// ajax.onreadystatechange = function () {
-//   if (ajax.readyState == 4 && ajax.status == 200) {
-//     console.log(ajax.responseText);
-//   }
-// };
-
-
+// fetch para mostrar nombres de usuarios
+const fetchUsers = async () => {
+  const res = await fetch("http://localhost:3000/users");
+  const data = await res.json();
+  return data;
+}
